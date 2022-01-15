9f3d8b23c022cebc0efd00817f395c6c8d491cd9
Yury Amelchanka <yury.amelchanka@gmail.com>
2022-01-15 14:38:02 -0800

    updates

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "UTPExpression",
-
-            url: "https://github.com/anessence/KMMPackages/archive/refs/tags/0.0.3.zip",
+            url: "git@github.com/anessence/KMMPackages/archive/refs/tags/0.0.3.zip",
             checksum: "d709642b8d4753528212f218d588c6198291e3dbcd134c1f6ba8ee540cd2f9c5"
         ),
     ]
