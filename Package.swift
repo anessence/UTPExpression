d95875d82323448dd5d959339ec92bcc876ce366
Yury Amelchanka <yury.amelchanka@gmail.com>
2022-01-14 21:25:26 -0800

    init

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "UTPExpression",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "UTPExpression",
+            targets: ["UTPExpression"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UTPExpression",
+
+            url: "https://github.com/anessence/KMMPackages/archive/refs/tags/0.0.3.zip",
+            checksum: "d709642b8d4753528212f218d588c6198291e3dbcd134c1f6ba8ee540cd2f9c5"
+        ),
+    ]
+)
